66e3fc1f66bba5b027ee072bb710497e2a3f176d
abdulbari149 <abdulbari131103@gmail.com>
2022-01-19 22:36:37 +0500

    Updating warden controller

--- back-end/controller/WardenController.js
+++ back-end/controller/WardenController.js
@@ -1,54 +1,55 @@
-const { Warden } = require("../models")
-const { compare } = require("bcrypt")
-const response = {
-    message: "",
-    status: 0, 
-    data: null
-}
+const { Warden } = require("../models");
+const { compare, hash } = require("bcrypt");
+
 // Authorized --> 100
-class WardenController{
-    async authLogin(req, res){
-        const { warden } = req.params
-        login: try {
-            const dbWarden = await Warden.findOne({ wardenId: warden.wardenId })
-            if(!dbWarden) {
-                response.message = "You have passed the wrong id"
-                response.status = 0
-                break login
-            }
+class WardenController {
+  response = { message: "", status: 0, data: null };
 
-            const isAuthorized = await compare(warden.password, dbWarden.password)
-
-            if(!isAuthorized) {
-                response.message = "You have entered a wrong password"
-                response.status = 0
-                break login
-            }
-            
-            response.message = "Warden is authorized to access the application"
-            response.message = 100
-            response.data = dbWarden
-            
-        } catch (error) {
-            console.error(error)
-        } finally{
-            res.send(response)
-        }
-    
+  async authLogin(req, res) {
+    const { warden } = req.params;
+    login: try {
+      const dbWarden = await Warden.findOne({ wardenId: warden.wardenId });
+      if (!dbWarden) {
+        this.response.message = "You have passed the wrong id";
+        this.response.status = 0;
+        break login;
+      }
+      const isAuthorized = await compare(warden.password, dbWarden.password);
+      if (!isAuthorized) {
+        this.response.message = "You have entered a wrong password";
+        this.response.status = 0;
+        break login;
+      }
+      this.response.message = "Warden is authorized to access the application";
+      this.response.message = 100;
+      this.response.data = dbWarden;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      res.send(response);
     }
+  }
 
-    async registerWarden(req, res){
-        const { warden } = req.params
-        try {           
-            const newWarden = await Warden.create(warden)
-
-
-
-
-        } catch (error) {
-            
-        }
+  async registerWarden(req, res) {
+    const { warden } = req.params;
+    try {
+      const hashedPassword = await hash(warden.password, 10)
+      const wardenDoc = new Warden({
+        name: warden.name,
+        wardenId: warden.wardenId,
+        password: hashedPassword
+      });
+      
+      const data = await wardenDoc.save();
+      this.response.message = "Successfully Saved"
+      this.response.status = 200
+      this.response.data = data
+    } catch (error) {
+      this.response.message = "Error Occured!",
+      this.response.status = 404
+      this.response.data = error
     }
+  }
 }
 
-module.exports = new WardenController()
+module.exports = new WardenController();
