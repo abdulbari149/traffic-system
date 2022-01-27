f8c23f7b6f888409076d53652f635b6dce352c64
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2022-01-27 23:31:46 +0500

    Add two more citizen controllers

--- back-end/routes/CitizenRoutes.js
+++ back-end/routes/CitizenRoutes.js
@@ -1,14 +1,14 @@
 const { Router } = require("express");
-const {CitizenController} = require("../controllers");
+const { CitizenController } = require("../controllers");
 const router = Router();
 
-router.post('/auth/login', CitizenController.authLogin);
+router.post('/auth/login', CitizenController.loginCitizen);
 // /api/citizen/auth/login 
 
 router.post('/auth/register', CitizenController.registerCitizen);
 // /api/citizen/auth/register 
 
-router.get('/:id', CitizenController.getCitizenData);
+router.get('/:id', CitizenController.getCitizen);
 // /api/citizen/auth/:id 
 
 module.exports = router;
