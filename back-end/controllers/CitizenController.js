c6d7b99083f8306c352433d977f91728ab985e54
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2022-01-27 23:31:46 +0500

    Add two more citizen controllers

--- back-end/controllers/CitizenController.js
+++ back-end/controllers/CitizenController.js
@@ -1,7 +1,7 @@
 const { compare, hash } = require("bcrypt");
 const Citizen = require("../models/CitizenModel");
 
-exports.login = async ({body: {citizen}}, res) => {
+exports.loginCitizen = async ({ body: { citizen } }, res) => {
     try {
         const foundCitizen = await Citizen.findOne({ email: citizen.email });
 
@@ -21,14 +21,62 @@ exports.login = async ({body: {citizen}}, res) => {
         }
 
         res.status(200).send({
-            status: "200",
+            status: 200,
             message: "You are authorized to use our service",
             data: foundCitizen
         })
 
     } catch (error) {
-        console.error(error)
+        res.status(400).send({
+            status: 400,
+            message: "Error occured!",
+            error
+        })
+        console.error(error);
     }
 }
 
+exports.registerCitizen = async ({ body: { citizen } }, res) => {
+    const { name, cnic, email, password, phone_number, city, country, address } = citizen;
+    try {
+        const hashedPassword = await hash(password, 10);
+        const citizenDetails = new Citizen({ name, cnic, email, password: hashedPassword, phone_number, city, country, address });
+        const accountAdded = await citizenDetails.save();
+        res.status(201).send({
+            status: 201,
+            message: "Successfully saved!",
+            data: accountAdded
+        })
+    } catch (error) {
+        res.status(400).send({
+            status: 400,
+            message: "Error occured!",
+            error
+        })
+        console.error(error);
+    }
+}
 
+exports.getCitizen = async ({params: {id}}, res) => {
+    try {
+        const foundCitizen = await Citizen.findOne({_id: id});
+        if (!foundCitizen) {
+            res.status(404).send({
+                status: 404,
+                message: "You have passed wrong ID."
+            });
+        }
+        res.status(200).send({
+            status: 200,
+            message: "Successfully fetched!",
+            data: foundCitizen
+        })
+    } catch (error) {
+        res.status(400).send({
+            status: 400,
+            message: "Error occured!",
+            error
+        })
+        console.error(error);
+    }
+}
