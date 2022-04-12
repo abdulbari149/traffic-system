eaf27ee8d80dcdd62731cc37b12f0960ad08c2b3
abdulbari149 <abdulbari131103@gmail.com>
2022-04-12 17:20:18 +0500

    Adding Authorize and Decline API

--- back-end/routes/WardenRoutes.js
+++ back-end/routes/WardenRoutes.js
@@ -1,15 +1,41 @@
 const { Router } = require("express");
 const { WardenController, ImageController } = require("../controllers");
+const { authorizeValidator } = require("../helpers/validators");
 const router = Router();
 const upload = require("../lib/imageUpload");
 const { verifyAuthToken } = require("../middlewares/authTokenVerification");
+const {
+  validationRequestSchema,
+} = require("../middlewares/validationRequestSchema");
+const { verifyAdmin } = require("../middlewares/verifyAdmin");
+
 router.post("/upload-image", upload, ImageController.uploadImages);
 router.get(
   "/challan-count",
   verifyAuthToken,
   WardenController.getWardenChallanCount
 );
-router.get("/approval", WardenController.getWardenListForApproval);
+router.get(
+  "/approval",
+  verifyAuthToken,
+  verifyAdmin,
+  WardenController.getWardenListForApproval
+);
 router.get("/approval/:id", WardenController.getWardenDetailsById);
-
+router.post(
+  "/authorize",
+  authorizeValidator(),
+  validationRequestSchema,
+  verifyAuthToken,
+  verifyAdmin,
+  WardenController.authorizeWarden
+);
+router.post(
+  "/decline",
+  authorizeValidator(),
+  validationRequestSchema,
+  verifyAuthToken,
+  verifyAdmin,
+  WardenController.declineWarden
+);
 module.exports = router;
--- dashboard-frontend/src/components/Dashboard.jsx
+++ dashboard-frontend/src/components/Dashboard.jsx
@@ -56,62 +56,6 @@ const Dashboard = () => {
       <Sidebar menuOpened={menuOpened} isMenuOpened={isMenuOpened} />
       <Outlet />
     </Grid>
-    // nav === 0 ? (
-    //   <>
-    //     <WardenApprovalPage
-    //       matches={matches}
-    //       id={id}
-    //       handleIdChange={handleIdChange}
-    //     />
-    //     {matches && id ? (
-    //
-    //     ) : null}
-    //   </>
-    // ) : nav === 1 ? (
-    //   <Grid
-    //     item
-    //     md={matches ? 10.5 : 12}
-    //     lg={matches ? 10.5 : 12}
-    //     sm={matches ? 10.5 : 12}
-    //     xs={12}
-    //   >
-    //     <Violation matches={matches} />
-    //   </Grid>
-    // ) : nav === 2 ? (
-    //   <>
-    //     <Grid
-    //       item
-    //       md={matches && id ? 7.5 : matches && !id ? 10.5 : 12}
-    //       lg={matches && id ? 7.5 : !matches ? 12 : 10.5}
-    //       sm={matches && !id ? 10.5 : matches && id ? 7.5 : 12}
-    //       xs={12}
-    //     >
-    //       <DeclinedWardens
-    //         matches={matches}
-    //         handleIdChange={handleIdChange}
-    //       />
-    //     </Grid>
-    //     {matches && id ? (
-    //       <Grid item xs={3}>
-    //         <WardenProfile
-    //           handleIdChange={handleIdChange}
-    //           matches={matches}
-    //         />
-    //       </Grid>
-    //     ) : null}
-    //   </>
-    // ) : nav === 3 ? (
-    //   <Grid
-    //     item
-    //     md={matches ? 10.5 : 12}
-    //     lg={matches ? 10.5 : 12}
-    //     sm={matches ? 10.5 : 12}
-    //     xs={12}
-    //   >
-    //     <SuperAdminSignUp />
-    //   </Grid>
-    // ) : null}
-    // </Grid>
   );
 };
 
